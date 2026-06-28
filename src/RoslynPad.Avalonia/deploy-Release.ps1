acb28165a75d08c02b348aee11621770e52997a2
Nathaniel A Collier <drxs3v3n@gmail.com>
2026-06-28 09:52:44 -0500

    Stop nuget from treating upstream package vulnerabilities as build failures
    Add the deploy script

---
+++ src/RoslynPad.Avalonia/deploy-Release.ps1
@@ -0,0 +1,23 @@
+﻿
+# default it to windows
+$runtimeConfig = "win-x64"
+
+if ($IsLinux) {
+    $runtimeConfig = "linux-x64"
+}
+Write-Host -ForegroundColor Blue -Object "Using runtime [$runtimeConfig]"
+
+$projectFileInfo = Get-ChildItem -Path $PSScriptRoot | where { $_.Extension -eq ".csproj"} | select -First 1
+
+$deployDir = [system.io.directory]::CreateDirectory(  [System.IO.Path]::Combine([system.environment]::GetFolderPath("userprofile"), "programs", [system.io.path]::GetFileNameWithoutExtension($projectFileInfo.Name) ))
+
+# clear existing folder
+remove-item ([system.io.path]::Combine($deployDir.FullName, "*")) -Recurse -Force
+
+$buildConfig = "Release"
+
+& dotnet @("publish", $projectFileInfo.FullName,  "-r", $runtimeConfig ,"-c", $buildConfig, "-o", 
+            $deployDir.FullName , "-p:PublishSingleFile=true",
+		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true"
+ )
+
